c8e2bd4d4ea3595d6c59ffb24531b6bdd19102fa
IT-ACA <65686992+IT-ACA@users.noreply.github.com>
2021-08-06 15:06:30 -0400

    Changed port to 3000 -  automatic deploy to Digital Ocean test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const http = require('http');
-const port = process.env.PORT || 3005;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! Push success 2!!! \n'
+  const msg = 'Hello Node! Push success 3!!! \n'
   res.end(msg);
 });
 
